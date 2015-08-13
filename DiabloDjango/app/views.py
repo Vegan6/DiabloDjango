58bc1a345575bf02eed87008651b520e382c9e34
Heretic <vegan6@gmail.com>
2015-08-12 20:14:00 -0600

    Test

--- DiabloDjango/app/views.py
+++ DiabloDjango/app/views.py
@@ -1,4 +1,4 @@
-﻿"""
+"""
 Definition of views.
 """
 
@@ -9,21 +9,25 @@ from datetime import datetime
 
 from DiabloDjango import DiabloAPI
 
+
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
     HeroProfile = DiabloAPI.HeroProfile(DiabloAPI.US_SERVER, 'Heretic-1984', '64346468')
-    query = request.GET.get('hello','')
+    query = request.GET.get('hello', '')
     return render(
         request,
         'app/index.html',
-        context_instance = RequestContext(request,
+        context_instance=RequestContext(request,
         {
-            'title':'Diablo 3',
-            'year':datetime.now().year,
-            'HeroProfile': str(query) + "Hero Name: " + HeroProfile['name'] + " Paragon Level:" + str(HeroProfile['paragonLevel']),
+            'title': 'Diablo 3',
+            'year': datetime.now().year,
+            'HeroProfile': str(query)
+                + "Hero Name: " + HeroProfile['name']
+                + " Paragon Level:" + str(HeroProfile['paragonLevel']),
         })
     )
+
 
 def contact(request):
     """Renders the contact page."""
@@ -31,13 +35,14 @@ def contact(request):
     return render(
         request,
         'app/contact.html',
-        context_instance = RequestContext(request,
+        context_instance=RequestContext(request,
         {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
+            'title': 'Contact',
+            'message': 'Your contact page.',
+            'year': datetime.now().year,
         })
     )
+
 
 def about(request):
     """Renders the about page."""
@@ -45,10 +50,10 @@ def about(request):
     return render(
         request,
         'app/about.html',
-        context_instance = RequestContext(request,
+        context_instance=RequestContext(request,
         {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
+            'title': 'About',
+            'message': 'Your application description page.',
+            'year': datetime.now().year,
         })
     )
